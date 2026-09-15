0ddbc3f8a57961ce9be82cd03a1774c22ce999b3
AYOMIDE JESSE <ayomidejesse7@gmail.com>
2026-09-15 21:05:50 +0100

    Do not block requests on webhook registration

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 import type { IncomingMessage, ServerResponse } from 'node:http';
 import { createApplication } from '../src/app.js';
 
-const { appPromise, webhookRegistrationPromise } = createApplication();
+const { appPromise, logger, webhookRegistrationPromise } = createApplication();
+
+void webhookRegistrationPromise.catch((error: unknown) => {
+  logger.warn({ error }, 'Telegram webhook registration failed');
+});
 
 export default async function handler(request: IncomingMessage, response: ServerResponse) {
   const app = await appPromise;
-  await webhookRegistrationPromise;
   await app.ready();
   app.server.emit('request', request, response);
 }
